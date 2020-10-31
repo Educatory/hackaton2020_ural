7f37a5b2feb2757f89151fbbc7e4b87daa25997c
des1 <salavatsharapov@gmail.com>
2020-11-01 02:02:22 +0500

    criteria list

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
-from base.views import MunicipalityList, MunicipalityDetail
+from base.views import MunicipalityList, MunicipalityDetail, CriteriaList
+
 
 urlpatterns = [
     path('municipality/', include([
         path('list/', MunicipalityList.as_view(), name='list'),
         path('<int:pk>/', MunicipalityDetail.as_view(), name='detail'),
+
+        path('criteria/', CriteriaList.as_view(), name='criteria_list'),
+
     ])),
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
-from base.models import Municipality
+from base.models import Municipality, Criteria
 from core.views import ContextProcessor
 
 
@@ -13,8 +13,15 @@ class MunicipalityList(ListView, ContextProcessor):
     context_object_name = 'items'
 
 
-class MunicipalityDetail(DetailView):
+class MunicipalityDetail(DetailView, ContextProcessor):
     template_name = 'base/municipality_detail.html'
     section = 'municipality'
     page_title = '<i class="icon-map5 mr-2"></i> Районы/Города'
     model = Municipality
+
+
+class CriteriaList(ListView, ContextProcessor):
+    template_name = 'base/criteria_detail.html'
+    section = 'criteria'
+    page_title = '<i class="icon-puzzle4 mr-2"></i> Критерии'
+    model = Criteria

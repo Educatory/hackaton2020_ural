d667e0efde82dac62e9cdb2161a6a26fdcd551fb
des1 <salavatsharapov@gmail.com>
2020-10-31 22:48:43 +0500

    add reprts

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from dashboard.views import DashboardView
+from dashboard.views import DashboardView, ReportsView
 
 
 urlpatterns = [
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path('reports/', ReportsView.as_view(), name='reports'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -15,3 +15,8 @@ class DashboardView(TemplateView, ContextProcessor):
     def crumbs(self):
         return []
 
+
+class ReportsView(TemplateView, ContextProcessor):
+    template_name = 'dashboard/reports_index.html'
+    section = 'reports'
+    page_title = '<i class="icon-stats-bars4 mr-2"></i> Отчеты'

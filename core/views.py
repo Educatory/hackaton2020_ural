1f35e72dfe96780ff3ae564a6cc31b3968238054
des1 <salavatsharapov@gmail.com>
2020-10-31 18:21:44 +0500

    fix

--- core/views.py
+++ core/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.base import ContextMixin
+
+
+class ContextProcessor(ContextMixin):
+
+    def get_context_data(self, **kwargs):
+        context = super(ContextProcessor, self).get_context_data(**kwargs)
+        context['page_title'] = getattr(self, 'page_title', None)
+        context['section'] = getattr(self, 'section', None)
+        return context
 
 
 class IndexPage(TemplateView):
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,14 @@
 
 from django.views.generic import TemplateView
 
+from core.views import ContextProcessor
 
-class DashboardView(TemplateView):
+
+class DashboardView(TemplateView, ContextProcessor):
 
     template_name = 'dashboard/dashboard.html'
     section = 'dashboard'
-    page_title = '<i class="icon-home4 mr-2"></i>Панель управления'
+    page_title = '<i class="icon-map5 mr-2"></i> Информационная доска'
 
     @property
     def crumbs(self):

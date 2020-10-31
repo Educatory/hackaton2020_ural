15fa0f11cd53c3d6746d86b799b4d077943ebcd7
AlexDev <alexbog80@gmail.com>
2020-11-01 03:11:51 +0500

    add index city

--- base/forms.py
+++ base/forms.py
@@ -1,9 +1,9 @@
-from base.models import Municipality
+from base.models import Municipality, Criteria
 from core.forms import SimpleModelFormBase
 
 
 class MunicipalityAdminForm(SimpleModelFormBase):
-    form_id = 'event_form'
+    form_id = 'municipality_form'
 
     def __init__(self, *args, **kwargs):
         self.form_title = kwargs.pop('form_title', None)
@@ -12,4 +12,16 @@ class MunicipalityAdminForm(SimpleModelFormBase):
 
     class Meta:
         model = Municipality
+        fields = ('__all__')
+
+
+class CriteriaAdminForm(SimpleModelFormBase):
+    form_id = 'criteria_form'
+
+    def __init__(self, *args, **kwargs):
+        self.form_title = kwargs.pop('form_title', None)
+        super(CriteriaAdminForm, self).__init__(*args, **kwargs)
+
+    class Meta:
+        model = Criteria
         fields = ('__all__')
--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
-from base.views import MunicipalityList, MunicipalityDetail, CriteriaList, MunicipalityAdd
+from base.views import MunicipalityList, MunicipalityDetail, CriteriaList, MunicipalityAdd, CriteriaAdd, CriteriaEdit
 
 urlpatterns = [
     path('municipality/', include([
         path('list/', MunicipalityList.as_view(), name='list'),
         path('<int:pk>/', MunicipalityDetail.as_view(), name='detail'),
         path('criteria/', CriteriaList.as_view(), name='criteria_list'),
+        path('criteria/add/', CriteriaAdd.as_view(), name='criteria_add'),
+        path('criteria/<int:pk>/', CriteriaEdit.as_view(), name='criteria_edit'),
         path('add/', MunicipalityAdd.as_view(), name='add'),
     ])),
 ]

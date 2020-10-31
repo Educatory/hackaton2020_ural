68cfef559bcdba493b657458818a2fdae156eadb
AlexDev <alexbog80@gmail.com>
2020-11-01 01:00:11 +0500

    add user

---
+++ users/models.py
@@ -0,0 +1,16 @@
+
+from django.db import models
+
+
+# Create your models here.
+class CustomUser(models.Model):
+    ROLE = (
+        [1, 'Администратор'],
+        [2, 'Сотрудник'],
+        [1, 'Руководитьель']
+    )
+
+    role = models.PositiveSmallIntegerField("Роль пользователя", choices=ROLE, default=1)
+
+    def __str__(self):
+        return self.get_role_display()
